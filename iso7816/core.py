e72b66a60938aee1ecfd84d075b6283a900bb164
Egor <egor21@gmail.com>
2018-04-09 23:39:21 +0300

    update core.py
    have done:
    - get_readers
    - connect_to_reader
    - get_atr
    - transmit

--- iso7816/core.py
+++ iso7816/core.py
@@ -8,15 +8,27 @@ import logging
 
 import iso7816def
 
+PCSC_LIB = "/lib/x86_64-linux-gnu/libpcsclite.so.1"
 
-class ScardEx(Exception):
-    def __init__(self):
-        pass
 
+class Iso7816Exception(Exception):
+    """
+    The base class for iso7816 exception
+    """
+
+    def __init__(self, msg='Iso7816 Exception'):
+        self.msg = msg
+
+class ScardIORequest(ctypes.Structure):
+    """
+    This class need to compatibility to direct call function form "libpcslite: lib
+    """
+    _fields_ = [('dwProtocol', ctypes.c_long),
+                ('cbPciLength', ctypes.c_long)]
 
 class Iso7816():
 
-    def __init__(self, path_to_lib):
+    def __init__(self, path_to_lib=PCSC_LIB):
         try:
             self.pcsc_lib = ctypes.CDLL(path_to_lib)
 
@@ -32,23 +44,20 @@ class Iso7816():
                                                      None,
                                                      ctypes.byref(self.hwnd_app_context))
 
-            if (rv & 0xFFFFFFFF) == iso7816def.SCARD_F_INTERNAL_ERROR:
-                pass
+            error_code = rv & 0xFFFFFFFF
+            error_msg = "[initialization]"
 
-            elif (rv & 0xFFFFFFFF) == iso7816def.SCARD_E_INVALID_PARAMETER:
-                pass
+            if error_code:
+                try:
 
-            elif (rv & 0xFFFFFFFF) == iso7816def.SCARD_E_NO_MEMORY:
-                pass
+                    error_msg += iso7816def.DSC_ERROR[error_code]
 
-            elif (rv & 0xFFFFFFFF) == iso7816def.SCARD_E_INVALID_VALUE:
-                pass
+                except KeyError:
+                    raise Iso7816Exception(error_msg + "Unknown error code: {:X}".format(error_code))
 
-            elif (rv & 0xFFFFFFFF) == iso7816def.SCARD_F_COMM_ERROR:
-                pass
+                else:
+                    raise Iso7816Exception(error_msg)
 
-            elif (rv & 0xFFFFFFFF) == iso7816def.SCARD_E_NO_SERVICE:
-                pass
 
             else:
                 # '__hwnd_reader' and '__protocol' used to communicate to C-library
@@ -60,25 +69,51 @@ class Iso7816():
                 self.hwnd_reader = None
                 self.protocol = None
 
-    def __check_rv(self, rv):
+    def __del__(self):
+        rv = self.pcsc_lib.SCardDisconnect(self.hwnd_reader, iso7816def.SCARD_UNPOWER_CARD)
+        # self.__check_rv(rv)
+
+    def __check_rv(self, rv, fun=" "):
         """
         Check value of "rv"
-        :param rv: returned value of PCSC library
+        :param rv:          returned value of PCSC library
+        :param fun:    "name of function, who call __check_rv
         :return: exception
         """
         error_code = rv & 0xFFFFFFFF
 
+        error_msg = '[' + fun + '] '
+
         if error_code:
             try:
-                error_msg = iso7816def.DSC_ERROR[error_code]
+                error_msg += iso7816def.DSC_ERROR[error_code]
 
             except KeyError:
-                print "Unknown error code: {:X}".format(error_code)
-                raise Exception
+                raise Iso7816Exception(error_msg + "Unknown error code: {:X}".format(error_code))
 
             else:
-                print error_msg
-                raise Exception
+                raise Iso7816Exception(error_msg)
+
+    def __check_sw(self, sw1, sw2):
+        """
+        Check status word
+        :param sw1:
+        :param sw2:
+        :return:
+        """
+        sw = (sw1 << 8) | sw2
+        err_msg = ""
+
+        if (sw1 != 0x61) and (sw1 != 0x90):
+            try:
+                err_msg = '[' + iso7816def.DSC_SW_ERROR[sw] + ']'
+
+            except KeyError:
+                raise Iso7816Exception("Unknown error code: {:X}".format(sw))
+
+            else:
+                # raise Iso7816Exception("[Error] {:X} ".format(sw) + err_msg)
+                raise Iso7816Exception(err_msg + " {:X} ".format(sw))
 
     def get_readers(self):
         """
@@ -89,13 +124,13 @@ class Iso7816():
 
         rv = self.pcsc_lib.SCardListReaders(self.hwnd_app_context, None, None, ctypes.byref(n_multistrings))
 
-        self.__check_rv(rv)
+        self.__check_rv(rv, self.get_readers.__name__)
 
         readers = ctypes.create_string_buffer(n_multistrings.value)
 
         rv = self.pcsc_lib.SCardListReaders(self.hwnd_app_context, None, readers, ctypes.byref(n_multistrings))
 
-        self.__check_rv(rv)
+        self.__check_rv(rv, self.get_readers.__name__)
 
         # print "Available readres: {}".format(readers.value)
         return readers.value
@@ -118,7 +153,7 @@ class Iso7816():
                                         ctypes.byref(self.__hwnd_reader),
                                         ctypes.byref(self.__protocol))
 
-        self.__check_rv(rv)
+        self.__check_rv(rv, self.connect_to_reader.__name__)
 
         self.hwnd_reader = self.__hwnd_reader.value
         self.protocol = self.__protocol.value
@@ -132,8 +167,8 @@ class Iso7816():
 
         reader_len = ctypes.c_long()
         state_reader = ctypes.c_long()
-        atr = ctypes.create_string_buffer(iso7816def.MAX_ATR_SIZE)      # Current ATR of a card in this reade
-        atr_len = ctypes.c_long(ctypes.sizeof(atr))                     # Length of ATR
+        atr = ctypes.create_string_buffer(iso7816def.MAX_ATR_SIZE)  # Current ATR of a card in this reade
+        atr_len = ctypes.c_long(ctypes.sizeof(atr))                 # Length of ATR
 
         rv = self.pcsc_lib.SCardStatus(self.__hwnd_reader,
                                        None,
@@ -143,6 +178,74 @@ class Iso7816():
                                        atr,
                                        ctypes.byref(atr_len))
 
-        self.__check_rv(rv)
+        self.__check_rv(rv, self.get_atr.__name__)
+
+        # print "state reader: ", hex(state_reader.value & 0xFFFFFFFF)
 
         return list(struct.unpack('%dB' % atr_len.value, atr[:atr_len.value]))
+
+    def __tx_rx_apdu(self, apdu, apdu_len):
+
+        # Structure of Protocol Control Information
+        pio_send_pci = ScardIORequest()
+        pio_send_pci.dwProtocol = self.__protocol
+
+        apdu_rx = ctypes.create_string_buffer(200)
+        apdu_rx_len = ctypes.c_long(ctypes.sizeof(apdu_rx))
+
+        rv = self.pcsc_lib.SCardTransmit(self.__hwnd_reader,
+                                         ctypes.byref(pio_send_pci),
+                                         apdu,
+                                         ctypes.c_long(apdu_len),
+                                         None,
+                                         apdu_rx,
+                                         ctypes.byref(apdu_rx_len))
+
+        self.__check_rv(rv, self.transmit.__name__)
+
+        raw_apdu = list(struct.unpack('%dB' % apdu_rx_len.value, apdu_rx[:apdu_rx_len.value]))
+
+        return raw_apdu
+
+    def transmit(self, raw_apdu=None):
+        """
+        Transmit apdu to smart-card and receive answer
+        :param raw_apdu:    may be string "90 AA BB..."
+                            or list [0x90, 0x85, 0x4F, ...]
+        :return:            apdu, sw1, sw2 or exception
+        """
+        if raw_apdu is None:
+            raise Iso7816Exception("Can't transmit empty 'APDU'")
+
+        if isinstance(raw_apdu, str):
+            raw_apdu = raw_apdu.split(" ")
+            raw_apdu = [int(x, 16) for x in raw_apdu]
+
+        apdu_len = len(raw_apdu)
+        apdu = struct.pack('%dB' % apdu_len, *raw_apdu)
+
+        sw = None
+        sc_response = []
+
+        while sw != 0x9000:
+            raw_rx_apdu = self.__tx_rx_apdu(apdu, apdu_len)
+
+            if raw_rx_apdu:
+                sw2 = raw_rx_apdu.pop()
+                sw1 = raw_rx_apdu.pop()
+
+                # if (sw1 != 0x90) or (sw1 != 0x61):
+                self.__check_sw(sw1, sw2)
+
+                sw = (sw1 << 8) | sw2
+                sc_response.extend(raw_rx_apdu)
+
+                if sw1 == 0x61:
+                    apdu = [0x00, 0xC0, 0x00, 0x00, sw2]
+                    apdu_len = len(apdu)
+                    apdu = struct.pack('%dB' % apdu_len, *apdu)
+            else:
+                raise Iso7816Exception("Wrong apdu")
+
+        return sc_response, sw1, sw2
+
